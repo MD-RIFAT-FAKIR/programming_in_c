c5a8a46c0b220248fb96f71f58c7d376ca114578
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2026-02-08 13:14:29 +0600

    datatypes and variable in c

---
+++ datatype_and_variables/datatypeAndVariable.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+    //integir type variable
+    int age = 24;
+
+    //fload type variabel
+    float height = 5.5;
+
+    //double type variable
+    double pi = 3.1416;
+
+    //character type variable
+    char  grade = 'A';
+
+    printf("Age : %d\n",age);
+    printf("Height : %.1f\n",height);
+    printf("PI : %.4lf\n",pi);
+    printf("Grade : %c\n",grade);
+
+
+    
+}
